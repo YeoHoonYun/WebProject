e65dd75d0269737a0011acd4b5a0e1db1b01e561
yun <cjswo9207@gmail.com>
2019-01-14 16:18:27 +0900

    docs : MVC 작성

---
+++ src/main/java/yun/fast/webproject/board/DAO/DBConnector.java
@@ -0,0 +1,8 @@
+package yun.fast.webproject.board.DAO;
+
+/**
+ * Created by cjswo9207u@gmail.com on 2019-01-14
+ * Github : https://github.com/YeoHoonYun
+ */
+public class DBConnector {
+}
---
+++ src/main/java/yun/fast/webproject/board/DTO/Board.java
@@ -0,0 +1,8 @@
+package yun.fast.webproject.board.DTO;
+
+/**
+ * Created by cjswo9207u@gmail.com on 2019-01-14
+ * Github : https://github.com/YeoHoonYun
+ */
+public class Board {
+}
